b7ace2abce8a59d3ff74ba156d97e9bfc3a720d2
Mirco Terenzi <116552555+mircoterenzi@users.noreply.github.com>
2025-10-15 13:18:52 +0200

    feat: add Position and Velocity components

---
+++ src/main/java/it/unibo/donkeykong/ecs/component/Position.java
@@ -0,0 +1,9 @@
+package it.unibo.donkeykong.ecs.component;
+
+/**
+ * Position component, which represents the position of an entity.
+ *
+ * @param x the x-coordinate
+ * @param y the y-coordinate
+ */
+public record Position(int x, int y) implements Component {}
