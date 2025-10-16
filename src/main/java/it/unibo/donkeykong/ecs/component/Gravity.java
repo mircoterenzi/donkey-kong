26ce67779efa092273ce192f4b55d42d6a17f602
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2025-10-16 12:45:08 +0200

    style: improved readability

--- src/main/java/it/unibo/donkeykong/ecs/component/Gravity.java
+++ src/main/java/it/unibo/donkeykong/ecs/component/Gravity.java
@@ -2,10 +2,7 @@ package it.unibo.donkeykong.ecs.component;
 
 import static it.unibo.donkeykong.utilities.Constants.GRAVITY;
 
-/**
- * Gravity component, which represents the gravitational force affecting an entity.
- *
- */
+/** Gravity component, which represents the gravitational force affecting an entity. */
 public class Gravity implements Component {
   private final double gravity;
 
