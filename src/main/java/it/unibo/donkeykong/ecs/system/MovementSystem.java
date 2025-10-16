20e8e62df078172609d6ad797cd8a88e70609877
Mirco Terenzi <116552555+mircoterenzi@users.noreply.github.com>
2025-10-16 11:14:45 +0200

    feat: implement movement logic in MovementSystem

--- src/main/java/it/unibo/donkeykong/ecs/system/MovementSystem.java
+++ src/main/java/it/unibo/donkeykong/ecs/system/MovementSystem.java
@@ -1,9 +1,32 @@
 package it.unibo.donkeykong.ecs.system;
 
 import it.unibo.donkeykong.ecs.World;
+import it.unibo.donkeykong.ecs.component.Position;
+import it.unibo.donkeykong.ecs.component.Velocity;
+import it.unibo.donkeykong.ecs.entity.Entity;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 public class MovementSystem implements GameSystem {
 
   @Override
-  public void update(World world, long deltaTime) {}
+  public void update(World world, long deltaTime) {
+    float secondsElapsed = deltaTime / 1000f;
+    Set<Entity> entitiesToMove =
+        world.getEntitiesWithComponents(List.of(Position.class, Velocity.class));
+
+    for (Entity entity : entitiesToMove) {
+      Optional<Position> startingPosition = entity.getComponent(Position.class);
+      Optional<Velocity> velocity = entity.getComponent(Velocity.class);
+
+      if (startingPosition.isPresent() && velocity.isPresent()) {
+        int updatedX = startingPosition.get().x() + (int) (velocity.get().dx() * secondsElapsed);
+        int updatedY = startingPosition.get().y() + (int) (velocity.get().dy() * secondsElapsed);
+
+        world.removeComponentFromEntity(entity, startingPosition.get());
+        world.addComponentToEntity(entity, new Position(updatedX, updatedY));
+      }
+    }
+  }
 }
