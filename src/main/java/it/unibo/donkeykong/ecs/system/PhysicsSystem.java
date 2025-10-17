86908d7055dc60f9965698c7ebdaabd713eaa2ea
Mirco Terenzi <116552555+mircoterenzi@users.noreply.github.com>
2025-10-17 17:40:38 +0200

    feat: implement collision response in PhysicsSystem

--- src/main/java/it/unibo/donkeykong/ecs/system/PhysicsSystem.java
+++ src/main/java/it/unibo/donkeykong/ecs/system/PhysicsSystem.java
@@ -2,6 +2,9 @@ package it.unibo.donkeykong.ecs.system;
 
 import it.unibo.donkeykong.ecs.World;
 import it.unibo.donkeykong.ecs.component.*;
+import it.unibo.donkeykong.ecs.entity.Entity;
+import java.util.List;
+import java.util.Set;
 
 /**
  * PhysicsSystem is responsible for handling physics-related updates in the game world, such as a
@@ -10,5 +13,19 @@ import it.unibo.donkeykong.ecs.component.*;
 public class PhysicsSystem implements GameSystem {
 
   @Override
-  public void update(World world, long deltaTime) {}
+  public void update(World world, long deltaTime) {
+    Set<Entity> movingEntities =
+        world.getEntitiesWithComponents(List.of(Velocity.class, CollisionEvent.class));
+    for (Entity movingEntity : movingEntities) {
+      Velocity velocity = movingEntity.getComponent(Velocity.class).orElseThrow();
+      CollisionEvent event = movingEntity.getComponent(CollisionEvent.class).orElseThrow();
+      boolean isBouncing = movingEntity.getComponent(Bounciness.class).isPresent();
+
+      world.removeComponentFromEntity(movingEntity, velocity);
+      world.removeComponentFromEntity(movingEntity, event);
+      world.removeComponentFromEntity(event.otherEntity(), event);
+      movingEntity.addComponent(
+          new Velocity(isBouncing ? -velocity.dx() : 0, isBouncing ? -velocity.dy() : 0));
+    }
+  }
 }
