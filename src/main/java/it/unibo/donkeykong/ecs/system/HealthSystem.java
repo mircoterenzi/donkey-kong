659bca6acc1acffc35127799d9166d8c0ff46272
Mirco Terenzi <116552555+mircoterenzi@users.noreply.github.com>
2025-10-17 18:03:24 +0200

    feat: add HealthSystem to manage entity health status

---
+++ src/main/java/it/unibo/donkeykong/ecs/system/HealthSystem.java
@@ -0,0 +1,9 @@
+package it.unibo.donkeykong.ecs.system;
+
+import it.unibo.donkeykong.ecs.World;
+
+/** HealthSystem manages the health status of entities within the game world. */
+public class HealthSystem implements GameSystem {
+  @Override
+  public void update(World world, long deltaTime) {}
+}
