4c7c2447493bd5d7a49053d24a95ad7dfb29cd14
Mirco Terenzi <116552555+mircoterenzi@users.noreply.github.com>
2025-10-18 00:13:27 +0200

    feat(health): implement HealthSystem to handle collision damage

--- src/main/java/it/unibo/donkeykong/ecs/system/HealthSystem.java
+++ src/main/java/it/unibo/donkeykong/ecs/system/HealthSystem.java
@@ -1,9 +1,29 @@
 package it.unibo.donkeykong.ecs.system;
 
 import it.unibo.donkeykong.ecs.World;
+import it.unibo.donkeykong.ecs.component.*;
+import it.unibo.donkeykong.ecs.entity.Entity;
+import java.util.List;
 
 /** HealthSystem manages the health status of entities within the game world. */
-public class HealthSystem implements GameSystem {
+public class HealthSystem extends CollisionEventHandlerSystem {
+
   @Override
-  public void update(World world, long deltaTime) {}
+  public void update(World world, long deltaTime) {
+    super.handleCollision(
+        world,
+        List.of(Health.class, CollisionEvent.class),
+        (entity) -> {
+          Health health = entity.getComponent(Health.class).orElseThrow();
+          Entity otherEntity =
+              entity.getComponent(CollisionEvent.class).orElseThrow().otherEntity();
+          if (otherEntity.getComponent(Damage.class).isPresent()) {
+            Damage damage = otherEntity.getComponent(Damage.class).orElseThrow();
+            world.removeComponentFromEntity(entity, health);
+            entity.addComponent(
+                new Health(
+                    health.livesCount() - damage.damageAmount())); // TODO: handle player death
+          }
+        });
+  }
 }
