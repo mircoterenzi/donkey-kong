494eff701a3e562a63f9166c094aab72da14c709
Mirco Terenzi <116552555+mircoterenzi@users.noreply.github.com>
2025-10-19 12:28:37 +0200

    feat(climbing): implement ClimbingSystem

--- src/main/java/it/unibo/donkeykong/ecs/system/ClimbingSystem.java
+++ src/main/java/it/unibo/donkeykong/ecs/system/ClimbingSystem.java
@@ -1,10 +1,29 @@
 package it.unibo.donkeykong.ecs.system;
 
 import it.unibo.donkeykong.ecs.World;
+import it.unibo.donkeykong.ecs.component.*;
+import it.unibo.donkeykong.ecs.entity.Entity;
+import java.util.List;
 
 /** ClimbingSystem handles the climbing mechanics for entities in the game world. */
-public class ClimbingSystem implements GameSystem {
+public class ClimbingSystem extends CollisionEventHandlerSystem {
 
   @Override
-  public void update(World world, long deltaTime) {}
+  public void update(World world, long deltaTime) {
+    super.handleCollision(
+        world,
+        List.of(Position.class, CollisionEvent.class),
+        (entity) -> {
+          Entity otherEntity =
+              entity.getComponent(CollisionEvent.class).orElseThrow().otherEntity();
+          if (otherEntity.getComponent(Climbable.class).isPresent()) {
+            Position entityPos = entity.getComponent(Position.class).orElseThrow();
+            Position otherPos = otherEntity.getComponent(Position.class).orElseThrow();
+            if (entityPos.x() == otherPos.x()) {
+              Gravity gravity = entity.getComponent(Gravity.class).orElseThrow();
+              entity.removeComponent(gravity);
+            }
+          }
+        });
+  }
 }
