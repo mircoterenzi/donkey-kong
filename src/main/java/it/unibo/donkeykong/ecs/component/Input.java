bf35cecc92884aacee01249b80936fc793fc685d
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2025-10-22 11:55:20 +0200

    feat: implement Input and InputProcessorSystem for handling player input

---
+++ src/main/java/it/unibo/donkeykong/ecs/component/Input.java
@@ -0,0 +1,41 @@
+package it.unibo.donkeykong.ecs.component;
+
+public class Input implements Component {
+  public enum HorizontalInput { MOVE_LEFT, MOVE_RIGHT, NONE }
+  public enum VerticalInput { MOVE_UP, MOVE_DOWN, NONE }
+
+  private HorizontalInput currentHInput;
+  private VerticalInput currentVInput;
+
+  private boolean jumpPressed;
+
+  public Input() {
+    this.currentHInput = HorizontalInput.NONE;
+    this.currentVInput = VerticalInput.NONE;
+    this.jumpPressed = false;
+  }
+
+  public HorizontalInput getCurrentHInput() {
+    return currentHInput;
+  }
+
+  public void setCurrentHInput(final HorizontalInput currentHInput) {
+    this.currentHInput = currentHInput;
+  }
+
+  public VerticalInput getCurrentVInput() {
+    return currentVInput;
+  }
+
+  public void setCurrentVInput(final VerticalInput currentVInput) {
+    this.currentVInput = currentVInput;
+  }
+
+  public boolean isJumpPressed() {
+    return jumpPressed;
+  }
+
+  public void setJumpPressed(final boolean jumpPressed) {
+    this.jumpPressed = jumpPressed;
+  }
+}
