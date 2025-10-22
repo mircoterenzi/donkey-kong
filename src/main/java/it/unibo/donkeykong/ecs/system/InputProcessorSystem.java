3d5322e53e339eda35d7e191fa6cd02648498ba2
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2025-10-22 23:25:53 +0200

    feat: refactor InputProcessorSystem logic

--- src/main/java/it/unibo/donkeykong/ecs/system/InputProcessorSystem.java
+++ src/main/java/it/unibo/donkeykong/ecs/system/InputProcessorSystem.java
@@ -1,11 +1,10 @@
 package it.unibo.donkeykong.ecs.system;
 
+import static it.unibo.donkeykong.utilities.Constants.*;
+
 import it.unibo.donkeykong.ecs.World;
 import it.unibo.donkeykong.ecs.component.*;
-
 import java.util.List;
-
-import static it.unibo.donkeykong.utilities.Constants.*;
 
 /** System that processes player input and updates entity velocities accordingly. */
 public class InputProcessorSystem implements GameSystem {
@@ -20,33 +19,48 @@ public class InputProcessorSystem implements GameSystem {
               Velocity oldVelocity = entity.getComponent(Velocity.class).orElseThrow();
 
               double newDx, newDy;
-              final boolean isClimbing = world.getEntitiesWithComponents(List.of(Climbable.class)).stream()
-                .anyMatch(e -> e.getComponent(CollisionEvent.class).orElseThrow().otherEntity().equals(entity));
-              final boolean isGrounded = world.getEntitiesWithComponents(List.of(Position.class)).stream()
-                .filter(e -> !e.equals(entity))
-                .anyMatch(e -> e.getComponent(CollisionEvent.class).orElseThrow().otherEntity().equals(entity));
+              final boolean isClimbing =
+                world.getEntitiesWithComponents(List.of(Climbable.class)).stream()
+                  .anyMatch(
+                    e ->
+                      e.getComponent(CollisionEvent.class)
+                        .orElseThrow()
+                        .otherEntity()
+                        .equals(entity));
+              final boolean isGrounded =
+                world.getEntitiesWithComponents(List.of(Position.class)).stream()
+                  .filter(e -> !e.equals(entity))
+                  .anyMatch(
+                    e ->
+                      e.getComponent(CollisionEvent.class)
+                        .orElseThrow()
+                        .otherEntity()
+                        .equals(entity));
 
               newDx =
-                  switch (input.getCurrentHInput()) {
-                    case MOVE_LEFT -> -DX_PLAYER_VELOCITY;
-                    case MOVE_RIGHT -> DX_PLAYER_VELOCITY;
-                    default -> 0;
-                  };
+                switch (input.getCurrentHInput()) {
+                  case MOVE_LEFT -> -DX_PLAYER_VELOCITY;
+                  case MOVE_RIGHT -> DX_PLAYER_VELOCITY;
+                  default -> 0;
+                };
 
-              if (isClimbing && !input.isJumpPressed()) {
+              if (input.isJumpPressed()) {
+                if (isGrounded || isClimbing)
+                  newDy = -DY_PLAYER_VELOCITY * 2;
+                else
+                  newDy = oldVelocity.dy();
+                input.setJumpPressed(false);
+              } else if (isGrounded) {
+                newDy = 0;
+              } else if (isClimbing) {
                 newDy =
                     switch (input.getCurrentVInput()) {
                       case MOVE_UP -> -DY_PLAYER_VELOCITY;
                       case MOVE_DOWN -> DY_PLAYER_VELOCITY;
                       default -> 0;
                     };
-              } else if (input.isJumpPressed() && (isGrounded || isClimbing)) {
-                newDy = -DY_PLAYER_VELOCITY * 2;
-                input.setJumpPressed(false);
-              } else if (input.getCurrentVInput() == Input.VerticalInput.MOVE_DOWN && !isGrounded) {
+              } else if (input.getCurrentVInput() == Input.VerticalInput.MOVE_DOWN) {
                 newDy = DY_PLAYER_VELOCITY * GRAVITY;
-              } else if (isGrounded){
-                newDy = 0;
               } else {
                 newDy = oldVelocity.dy();
               }
