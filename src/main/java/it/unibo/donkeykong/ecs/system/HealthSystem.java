55c6c36ba3870b90eff3dec5bc41ae1c33609168
Mirco Terenzi <116552555+mircoterenzi@users.noreply.github.com>
2025-10-24 09:49:36 +0200

    refactor: removed CollisionEventHandlerSystem

--- src/main/java/it/unibo/donkeykong/ecs/system/HealthSystem.java
+++ src/main/java/it/unibo/donkeykong/ecs/system/HealthSystem.java
@@ -4,26 +4,27 @@ import it.unibo.donkeykong.ecs.World;
 import it.unibo.donkeykong.ecs.component.*;
 import it.unibo.donkeykong.ecs.entity.Entity;
 import java.util.List;
+import java.util.Set;
 
 /** HealthSystem manages the health status of entities within the game world. */
-public class HealthSystem extends CollisionEventHandlerSystem {
+public class HealthSystem implements GameSystem {
 
   @Override
   public void update(World world, long deltaTime) {
-    super.handleCollision(
-        world,
-        List.of(Health.class, CollisionEvent.class),
-        (entity) -> {
-          Health health = entity.getComponent(Health.class).orElseThrow();
-          Entity otherEntity =
-              entity.getComponent(CollisionEvent.class).orElseThrow().otherEntity();
-          if (otherEntity.getComponent(Damage.class).isPresent()) {
-            Damage damage = otherEntity.getComponent(Damage.class).get();
-            world.removeComponentFromEntity(entity, health);
-            entity.addComponent(
-                new Health(
-                    health.livesCount() - damage.damageAmount())); // TODO: handle player death
-          }
-        });
+    Set<Entity> targetEntities =
+        world.getEntitiesWithComponents(
+            List.of(Health.class, CollisionEvent.class, CollisionEvent.class));
+    for (Entity entity : targetEntities) {
+      Health health = entity.getComponent(Health.class).orElseThrow();
+      CollisionEvent event = entity.getComponent(CollisionEvent.class).orElseThrow();
+      Entity otherEntity = event.otherEntity();
+      if (otherEntity.getComponent(Damage.class).isPresent()) {
+        Damage damage = otherEntity.getComponent(Damage.class).get();
+        world.removeComponentFromEntity(entity, health);
+        entity.addComponent(
+            new Health(health.livesCount() - damage.damageAmount())); // TODO: handle player death
+      }
+      world.removeComponentFromEntity(entity, event);
+    }
   }
 }
