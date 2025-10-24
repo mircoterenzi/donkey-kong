cb3f5ef97bab88fd087c8bbc32f209b38145980b
Mirco Terenzi <116552555+mircoterenzi@users.noreply.github.com>
2025-10-24 15:29:39 +0200

    feat(collision): define world dimensions

--- src/main/java/it/unibo/donkeykong/utilities/Constants.java
+++ src/main/java/it/unibo/donkeykong/utilities/Constants.java
@@ -1,5 +1,7 @@
 package it.unibo.donkeykong.utilities;
 
 public class Constants {
+  public static final int WORLD_WIDTH = 896;
+  public static final int WORLD_HEIGHT = 1024;
   public static final double GRAVITY = 9.81;
 }
