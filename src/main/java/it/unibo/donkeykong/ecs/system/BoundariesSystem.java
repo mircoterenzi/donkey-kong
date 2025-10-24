f7966ac5dce0d1db79eb34a4e930b2e9a17df5e3
Mirco Terenzi <116552555+mircoterenzi@users.noreply.github.com>
2025-10-24 15:30:27 +0200

    feat(collision): add BoundariesSystem for boundary management

---
+++ src/main/java/it/unibo/donkeykong/ecs/system/BoundariesSystem.java
@@ -0,0 +1,9 @@
+package it.unibo.donkeykong.ecs.system;
+
+import it.unibo.donkeykong.ecs.World;
+
+public class BoundariesSystem implements GameSystem {
+
+  @Override
+  public void update(World world, long deltaTime) {}
+}
