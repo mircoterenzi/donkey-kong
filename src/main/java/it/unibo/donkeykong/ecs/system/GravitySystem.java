6840474d86d8352c2862f71f0040f9efa1f70599
Mirco Terenzi <116552555+mircoterenzi@users.noreply.github.com>
2025-10-27 12:01:15 +0100

    refactor(gravity): remove unnecessary component from entity retrieval

--- src/main/java/it/unibo/donkeykong/ecs/system/GravitySystem.java
+++ src/main/java/it/unibo/donkeykong/ecs/system/GravitySystem.java
@@ -9,7 +9,7 @@ public class GravitySystem implements GameSystem {
   @Override
   public void update(World world, float deltaTime) {
     world
-        .getEntitiesWithComponents(List.of(Gravity.class, Velocity.class, Position.class))
+        .getEntitiesWithComponents(List.of(Gravity.class, Velocity.class))
         .forEach(
             entity -> {
               var gravity = entity.getComponent(Gravity.class).orElse(new Gravity(0));
