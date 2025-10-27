27abc1f4386f81980766b64bc0c6c56d6317c98d
Mirco Terenzi <116552555+mircoterenzi@users.noreply.github.com>
2025-10-27 12:01:33 +0100

    fix(gravity): velocity update

--- src/main/java/it/unibo/donkeykong/ecs/system/GravitySystem.java
+++ src/main/java/it/unibo/donkeykong/ecs/system/GravitySystem.java
@@ -14,8 +14,7 @@ public class GravitySystem implements GameSystem {
             entity -> {
               var gravity = entity.getComponent(Gravity.class).orElse(new Gravity(0));
               var velocity = entity.getComponent(Velocity.class).orElseThrow();
-
-              double newDy = velocity.dy() + gravity.gravity() * (deltaTime / 1000.0);
+              double newDy = velocity.dy() + gravity.gravity();
               Velocity newVelocity = new Velocity(velocity.dx(), newDy);
               entity.updateComponent(velocity, newVelocity);
             });
