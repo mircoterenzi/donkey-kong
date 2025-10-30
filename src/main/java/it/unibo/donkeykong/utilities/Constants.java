343cf160d9bae60b8007a312764e31803c6b19af
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2025-10-30 17:12:03 +0100

    feat(spawn): implement SpawnSystem for barrel spawning

--- src/main/java/it/unibo/donkeykong/utilities/Constants.java
+++ src/main/java/it/unibo/donkeykong/utilities/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
   public static final double GRAVITY = 9.81;
   public static final double JUMP_FACTOR = 5.0;
   public static final double FALL_FACTOR = 3.0;
-  public static final float SPAWN_INTERVAL = 2500;
+  public static final float SPAWN_INTERVAL = 3000f;
 
   public static final double PLAYER_VELOCITY = 7.0;
   public static final int PLAYER_WIDTH = 64;
