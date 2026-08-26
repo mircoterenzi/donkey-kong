8d279851fad4f5e9bc4f5504b58d80ebe08d07fe
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2026-08-26 17:55:17 +0200

    refactor(movement): remove unused MAX_VELOCITY constant and related logic

--- src/main/java/it/unibo/donkeykong/ecs/system/MovementSystem.java
+++ src/main/java/it/unibo/donkeykong/ecs/system/MovementSystem.java
@@ -11,9 +11,6 @@ import java.util.Set;
 
 /** MovementSystem updates the position of entities based on their velocity over time. */
 public class MovementSystem implements GameSystem {
-
-  private static final double MAX_VELOCITY = 200;
-
   @Override
   public void update(World world, float deltaTime) {
     Set<Entity> entitiesToMove =
@@ -24,16 +21,8 @@ public class MovementSystem implements GameSystem {
       Optional<VelocityComponent> velocity = entity.getComponent(VelocityComponent.class);
 
       if (startingPosition.isPresent() && velocity.isPresent()) {
-        // TODO: this should limit the max y-speed to impede tunneling (suboptimal).
-        if (velocity.get().dy() >= MAX_VELOCITY) {
-          VelocityComponent updatedVelocity =
-              new VelocityComponent(velocity.get().dx(), MAX_VELOCITY);
-          entity.updateComponent(updatedVelocity);
-          velocity = Optional.of(updatedVelocity);
-        }
         double updatedX = startingPosition.get().x() + (velocity.get().dx() * deltaTime);
         double updatedY = startingPosition.get().y() + (velocity.get().dy() * deltaTime);
-
         world.updateComponentOnEntity(entity, new PositionComponent(updatedX, updatedY));
       }
     }
