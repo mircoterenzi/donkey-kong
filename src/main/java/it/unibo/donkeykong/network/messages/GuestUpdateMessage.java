a4e17ca0983bc269ea8018dd48fb40633103196c
giacomofoschii <116550389+giacomofoschii@users.noreply.github.com>
2026-09-04 14:38:11 +0000

    style: auto format code with spotless

--- src/main/java/it/unibo/donkeykong/network/messages/GuestUpdateMessage.java
+++ src/main/java/it/unibo/donkeykong/network/messages/GuestUpdateMessage.java
@@ -1,15 +1,14 @@
 package it.unibo.donkeykong.network.messages;
 
 public record GuestUpdateMessage(
-  MessageType type,
-  String playerId,
-  double playerX,
-  double playerY,
-  String playerState,
-  String playerDirection
-) {
-  public GuestUpdateMessage(String playerId, double playerX, double playerY,
-                            String playerState, String playerDirection) {
+    MessageType type,
+    String playerId,
+    double playerX,
+    double playerY,
+    String playerState,
+    String playerDirection) {
+  public GuestUpdateMessage(
+      String playerId, double playerX, double playerY, String playerState, String playerDirection) {
     this(MessageType.GUEST_UPDATE, playerId, playerX, playerY, playerState, playerDirection);
   }
 }
