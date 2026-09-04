b002a987f7829313b39433d8c22f416a7291bd71
giacomofoschii <116550389+giacomofoschii@users.noreply.github.com>
2026-09-04 16:06:26 +0000

    style: auto format code with spotless

--- src/main/java/it/unibo/donkeykong/ecs/system/StateReceiverSystem.java
+++ src/main/java/it/unibo/donkeykong/ecs/system/StateReceiverSystem.java
@@ -8,7 +8,6 @@ import it.unibo.donkeykong.ecs.component.NetworkComponent;
 import it.unibo.donkeykong.ecs.component.PositionComponent;
 import it.unibo.donkeykong.ecs.component.StateComponent;
 import it.unibo.donkeykong.ecs.system.api.GameSystem;
-
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -29,14 +28,14 @@ public class StateReceiverSystem implements GameSystem {
   @Override
   public void update(World world, float deltaTime) {
     if ("GUEST".equals(myRole)) {
-      while(!hostUpdates.isEmpty()) {
+      while (!hostUpdates.isEmpty()) {
         JsonObject update = hostUpdates.poll();
         applyHostUpdate(world, update);
       }
     }
 
     if ("HOST".equals(myRole)) {
-      while(!guestUpdates.isEmpty()) {
+      while (!guestUpdates.isEmpty()) {
         JsonObject update = guestUpdates.poll();
         applyGuestUpdate(world, update);
       }
@@ -50,16 +49,20 @@ public class StateReceiverSystem implements GameSystem {
     String direction = update.getString("playerDirection");
 
     world.getEntitiesWithComponents(List.of(NetworkComponent.class)).stream()
-      .filter(e -> {
-        NetworkComponent net = e.getComponent(NetworkComponent.class).orElseThrow();
-        return "GUEST".equals(net.entityType());
-      })
-      .findFirst()
-      .ifPresent(guestEntity -> {
-        guestEntity.updateComponent(new PositionComponent(x, y));
-        guestEntity.updateComponent(new StateComponent(StateComponent.State.valueOf(state),
-          StateComponent.Direction.valueOf(direction)));
-      });
+        .filter(
+            e -> {
+              NetworkComponent net = e.getComponent(NetworkComponent.class).orElseThrow();
+              return "GUEST".equals(net.entityType());
+            })
+        .findFirst()
+        .ifPresent(
+            guestEntity -> {
+              guestEntity.updateComponent(new PositionComponent(x, y));
+              guestEntity.updateComponent(
+                  new StateComponent(
+                      StateComponent.State.valueOf(state),
+                      StateComponent.Direction.valueOf(direction)));
+            });
   }
 
   private void applyHostUpdate(World world, JsonObject update) {
@@ -69,31 +72,37 @@ public class StateReceiverSystem implements GameSystem {
     String direction = update.getString("playerDirection");
 
     world.getEntitiesWithComponents(List.of(NetworkComponent.class)).stream()
-      .filter(e -> {
-        NetworkComponent net = e.getComponent(NetworkComponent.class).orElseThrow();
-        return "HOST".equals(net.entityType());
-      })
-      .findFirst()
-      .ifPresent(hostEntity -> {
-        hostEntity.updateComponent(new PositionComponent(x, y));
-        hostEntity.updateComponent(new StateComponent(StateComponent.State.valueOf(state),
-          StateComponent.Direction.valueOf(direction)));
-      });
+        .filter(
+            e -> {
+              NetworkComponent net = e.getComponent(NetworkComponent.class).orElseThrow();
+              return "HOST".equals(net.entityType());
+            })
+        .findFirst()
+        .ifPresent(
+            hostEntity -> {
+              hostEntity.updateComponent(new PositionComponent(x, y));
+              hostEntity.updateComponent(
+                  new StateComponent(
+                      StateComponent.State.valueOf(state),
+                      StateComponent.Direction.valueOf(direction)));
+            });
 
     JsonArray barrels = update.getJsonArray("barrels");
-    if(barrels != null) {
+    if (barrels != null) {
       for (int i = 0; i < barrels.size(); i++) {
         JsonObject barrel = barrels.getJsonObject(i);
         String barrelId = barrel.getString("id");
         double barrelX = barrel.getDouble("x");
         double barrelY = barrel.getDouble("y");
 
-        var existingBarrel = world.getEntitiesWithComponents(List.of(NetworkComponent.class)).stream()
-          .filter(e -> {
-            NetworkComponent net = e.getComponent(NetworkComponent.class).orElseThrow();
-            return barrelId.equals(net.networkId());
-          })
-          .findFirst();
+        var existingBarrel =
+            world.getEntitiesWithComponents(List.of(NetworkComponent.class)).stream()
+                .filter(
+                    e -> {
+                      NetworkComponent net = e.getComponent(NetworkComponent.class).orElseThrow();
+                      return barrelId.equals(net.networkId());
+                    })
+                .findFirst();
 
         if (existingBarrel.isPresent()) {
           existingBarrel.get().updateComponent(new PositionComponent(barrelX, barrelY));
