f6f86e61038161cb8a0f593aec10481f4ec665d6
giacomofoschii <116550389+giacomofoschii@users.noreply.github.com>
2026-09-04 14:38:11 +0000

    style: auto format code with spotless

--- src/main/java/it/unibo/donkeykong/network/messages/BarrelData.java
+++ src/main/java/it/unibo/donkeykong/network/messages/BarrelData.java
@@ -1,7 +1,3 @@
 package it.unibo.donkeykong.network.messages;
 
-public record BarrelData(
-  String id,
-  double x,
-  double y
-) {}
+public record BarrelData(String id, double x, double y) {}
--- src/main/java/it/unibo/donkeykong/network/messages/GameStartMessage.java
+++ src/main/java/it/unibo/donkeykong/network/messages/GameStartMessage.java
@@ -1,8 +1,6 @@
 package it.unibo.donkeykong.network.messages;
 
-public record GameStartMessage(
-  MessageType type
-  ) {
+public record GameStartMessage(MessageType type) {
   public GameStartMessage() {
     this(MessageType.GAME_START);
   }
